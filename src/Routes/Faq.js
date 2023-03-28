096ac042e8a620ca14aef1c372b2a8bd2954450e
MohammadAli <galaxy.mrz8@gmail.com>
2023-03-29 03:06:18 +0430

    it have a problem and i dont know

---
+++ src/Routes/Faq.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import Question from "../Components/Faq"
+import Footer from '../Components/Footer';
+
+const Faq = () => {
+    return (
+        <>
+            <Navbar />
+            <Question />
+            <Footer />
+        </>
+    );
+};
+
+export default Faq;
