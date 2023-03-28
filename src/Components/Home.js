c2cf899b3472270bd71eae1280e48158f6ec1300
MohammadAli <galaxy.mrz8@gmail.com>
2023-03-29 02:08:41 +0430

    Pricing and TestimonailComponent added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 import Banner from "./Banner";
 import "./HomeStyles.css";
 import Navbar from "./Navbar";
+import Pricing from "./Pricing";
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Banner />
+      <Pricing/>
     </div>
   );
 };
